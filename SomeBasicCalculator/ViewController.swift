8e7e66d2df791bbd825d200a87eb591dcc455ac7
sylvandor <scottwwj@gmail.com>
2019-05-12 15:09:05 -0500

    Project creation. Figured I'd make a calculator. Seems like the thing to do.

---
+++ SomeBasicCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeBasicCalculator
+//
+//  Created by Scott Johnson on 5/12/19.
+//  Copyright © 2019 Scott Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
